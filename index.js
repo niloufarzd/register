65325a68fd9dedf6344589bc9a9da2f24e89e6ac
niloufar <zaferaniniloufar@gmail.com>
2022-01-02 09:41:36 +0330

    add project

---
+++ index.js
@@ -0,0 +1,9 @@
+//vraible
+const username=prompt("enter username");
+const password=prompt("enter password");
+ if(username.length < 3 || password.length <8){
+      alert ("you login")
+ }
+  else{
+      alert("error")
+  }
